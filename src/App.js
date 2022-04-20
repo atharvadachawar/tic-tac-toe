782bc5724b53e6e41f76d233efc5b4874f9b1a16
Atharva <a1008dachawar@gmail.com>
2022-04-20 09:10:50 +0530

    -am

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+
+function Square({ value, onClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
   );
 }
 
-export default App;
+function Restart({ onClick }) {
+  return (
+    <button className="restart" onClick="{onClick}">
+      Play Again
+    </button>
+  );
+}
+export default function Game() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const nextSymbol = isXNext ? "X" : "O";
+  const winner = calculateWinner(squares);
+
+  function renderSquare(i) {
+    return (
+      <>
+        <Square
+          value={squares[i]}
+          onClick={() => {
+            if (squares[i] != null || winner != null) {
+              return;
+            }
+            const nextSquares = squares.slice();
+            nextSquares[i] = nextSymbol;
+            setSquares(nextSquares);
+
+            setIsXNext(!isXNext);
+          }}
+        />
+      </>
+    );
+  }
+
+  const renderRestartButton = () => {
+    return (
+      <>
+        <Restart
+          onClick={() => {
+            setSquares(Array(9).fill(null));
+            setIsXNext(true);
+          }}
+        />
+      </>
+    );
+  };
+
+  function isBoardFull(squares) {
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] == null) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw";
+    } else {
+      return " Next player: " + nextSymbol;
+    }
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="game">
+          <div className="game-board">
+            <div className="board-row">
+              {renderSquare(0)}
+              {renderSquare(1)}
+              {renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {renderSquare(3)}
+              {renderSquare(4)}
+              {renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {renderSquare(6)}
+              {renderSquare(7)}
+              {renderSquare(8)}
+            </div>
+          </div>
+          <div className="game-info">{getStatus()}</div>
+          <div className="restart-button">{renderRestartButton()}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    } else {
+      return null;
+    }
+  }
+}
